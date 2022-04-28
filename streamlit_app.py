d804f177bdb836f7d1f4500993796d0400c0549e
NishantRaut777 <94901896+NishantRaut777@users.noreply.github.com>
2022-04-28 13:42:19 +0530

    Update streamlit_app.py
    Added loc method to get only selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-import pandas
+import pandas as pd
 
 streamlit.title('My Mom\'s healthy new diner')
 
@@ -12,10 +12,12 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect('Pick some Fruits:',list(my_fruit_list.index))
+fruits_selected = streamlit.multiselect('Pick some Fruits:',list(my_fruit_list.index))
 
-streamlit.dataframe(my_fruit_list)
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+streamlit.dataframe(fruits_to_show)
 
