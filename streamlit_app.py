27625a392d0c377314fbd4778f559341e02ff24f
NishantRaut777 <94901896+NishantRaut777@users.noreply.github.com>
2022-05-09 12:03:42 +0530

    normalized the json

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 
 streamlit.text(fruityvice_response.json())
 
+
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+
+streamlit.dataframe(fruityvice_normalized)
+
