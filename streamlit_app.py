8dd15db0c6e03d3a7baf891964ee4aa6f6822ce0
NishantRaut777 <94901896+NishantRaut777@users.noreply.github.com>
2022-05-09 12:05:31 +0530

    deleted json text

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,9 +25,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +"kiwi")
 
-streamlit.text(fruityvice_response.json())
+
 
 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
