f27366e7fd6a89c44883214c36dd82363200e21c
NishantRaut777 <94901896+NishantRaut777@users.noreply.github.com>
2022-05-09 12:08:24 +0530

    added choice at top

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,9 @@ import requests
 
 streamlit.header('Fruityvice Fruit Advice!')
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?' , 'Kiwi')
+streamlit.write('The user entered' , fruit_choice)
+
 streamlit.title('My Mom\'s healthy new diner')
 
 streamlit.header('Breakfast Favorites')
@@ -25,7 +28,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +"kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
 
