a420b345ca0e272f17b7680255ef6e656a104211
NishantRaut777 <94901896+NishantRaut777@users.noreply.github.com>
2022-05-10 00:31:11 -0500

    added streamlit.stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas as pd
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 
 streamlit.header('Fruityvice Fruit Advice!')
@@ -37,6 +38,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 
 streamlit.dataframe(fruityvice_normalized)
+
+
+streamlit.stop()
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
